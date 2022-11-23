be1c18aef8fce595d47dacee6713216b109c3b0d
Vegard Karlstrøm <vegard_overaas@hotmail.com>
2022-11-23 12:08:05 +0100

    Changed size, angle and color of plane

--- src/js/Objects/Plane.js
+++ src/js/Objects/Plane.js
@@ -1,13 +1,14 @@
 function Plane() {
     // Init PlaneGeometry object 
-    const geometry = new THREE.PlaneGeometry( 1, 1 );
+    const geometry = new THREE.PlaneGeometry( 12, 12 );
 
     // Create material with color
-    const material = new THREE.MeshBasicMaterial( {color: 0xffff00, side: THREE.DoubleSide} );
+    const material = new THREE.MeshBasicMaterial( {color: 0x0b520d, side: THREE.DoubleSide} );
 
     // Create mesh with geo and material
     const plane = new THREE.Mesh(geometry, material);
 
+    plane.rotateX(Math.PI / 2);
 
     return plane;
 }
