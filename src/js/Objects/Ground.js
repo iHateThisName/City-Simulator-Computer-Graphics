70ab61e2c6c4471e03a857ceb4b295f58761bcc6
Ivar <ivabr@stud.ntnu.no>
2022-11-23 15:03:05 +0100

    Created a plane that acts like the ground.

---
+++ src/js/Objects/Ground.js
@@ -0,0 +1,22 @@
+function ground() {
+
+    // Init PlaneGeometry object
+    const geometry = new THREE.PlaneGeometry(100, 100);
+
+    // Create material with color
+    const material = new THREE.MeshBasicMaterial({
+        color: 0x2a2b30,
+    });
+
+    // Create mesh with geo and material
+    const plane = new THREE.Mesh(geometry, material);
+
+    plane.rotateX(Math.PI / 2);
+    //Turning it upside, so we only need to render one side of the plane
+    plane.rotateY(Math.PI);
+    // Making the plane slightly lower to not collide with the park, Grid helper and Axes helper
+    plane.position.y = -0.1
+
+    return plane
+
+}
