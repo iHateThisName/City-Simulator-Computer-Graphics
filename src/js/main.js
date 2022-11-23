fdce06e52200853503477a621422c0bfaee198cc
Ivar <ivabr@stud.ntnu.no>
2022-11-23 10:46:18 +0100

    Added a small spinning cube.

---
+++ src/js/main.js
@@ -0,0 +1,49 @@
+// Init scene
+const scene = new THREE.Scene();
+
+// Init camera (PerspectiveCamera)
+const camera = new THREE.PerspectiveCamera
+(
+    75,
+    window.innerWidth / window.innerHeight,
+    0.1,
+    1000
+);
+
+// Init renderer
+const renderer = new THREE.WebGLRenderer({antialias: true});
+
+// Set size (whole window)
+renderer.setSize(window.innerWidth, window.innerHeight);
+
+// Render to canvas element
+document.body.appendChild(renderer.domElement);
+
+// Init BoxGeometry object (rectangular cuboid)
+const geometry = new THREE.BoxGeometry(1, 1, 1);
+
+// Create material with color
+const material = new THREE.MeshBasicMaterial({color: 0x0000ff});
+
+// Create mesh with geo and material
+const cube = new THREE.Mesh(geometry, material);
+
+// Add to scene
+scene.add(cube);
+
+// Position camera
+camera.position.z = 5;
+
+
+// Draw the scene every time the screen is refreshed
+function animate() {
+    requestAnimationFrame(animate);
+
+    // Rotate cube
+    cube.rotation.x += 0.005;
+    cube.rotation.y += 0.005;
+
+    renderer.render(scene, camera);
+}
+
+animate();
